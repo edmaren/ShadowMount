034e3bafab92d8076030c5e4f6442b5452c6ade1
VoidWhisper <voidwhisper.ps@gmail.com>
2026-01-08 02:47:02 +0000

    Remove unused source files

--- src/libSceNotification.c
+++
@@ -1,7 +0,0 @@
-asm(".global sceNotificationSend\n"
-    ".type sceNotificationSend @function\n"
-    "sceNotificationSend:\n");
-
-asm(".global sceNotificationSendById\n"
-    ".type sceNotificationSendById @function\n"
-    "sceNotificationSendById:\n");
